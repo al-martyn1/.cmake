72350ba3b8e9bf71e6efce959151f73257f64824
martynov_ag <martynov_ag@nectech.pro>
2025-12-24 19:14:49 +0300

    Последняя версия функции umba_fix_windres - всё равно не работает

--- umba-windres.cmake
+++ umba-windres.cmake
@@ -6,14 +6,17 @@ function(umba_fix_windres)
 # пути, которые содержат пробелы, разбиваются на отдельные аргументы, и препроцессор выдаёт ошибки
 # https://cmake.org/cmake/help/latest/command/find_program.html
 
-find_program(UMBA_WINDRES "umba-windres")
+find_program(UMBA_WINDRES_EXECUTABLE "umba-windres")
 
-message("CMAKE_RC_COMPILE_OBJECT: ${CMAKE_RC_COMPILE_OBJECT}")
+message("umba_fix_windres (1): CMAKE_RC_COMPILE_OBJECT: ${CMAKE_RC_COMPILE_OBJECT}")
 # Если тулчейн MINGW'шный и утилита umba-windres найдена, то фиксим, иначе пусть работает, как работало
-if(MINGW AND UMBA_WINDRES)
-    enable_language(RC)
-    message("MINGW AND UMBA_WINDRES fires")
-    message("UMBA_WINDRES: ${UMBA_WINDRES}")
+if(MINGW AND UMBA_WINDRES_EXECUTABLE)
+    #set(UMBA_WINDRES "${UMBA_WINDRES_EXECUTABLE}" CACHE STRING "umba-windres proxy" FORCE)
+    set(ENV{UMBA_WINDRES} "${UMBA_WINDRES_EXECUTABLE}")
+    # enable_language(RC)
+    # https://cmake.org/cmake/help/latest/manual/cmake-language.7.html#cmake-language-variables
+    message("umba_fix_windres: MINGW AND UMBA_WINDRES fires")
+    message("umba_fix_windres: UMBA_WINDRES: ${UMBA_WINDRES}")
     # https://man7.org/linux/man-pages/man1/windres.1.html
     set(CMAKE_RC_COMPILE_OBJECT 
         # "<CMAKE_RC_COMPILER> --use-temp-file -O coff <DEFINES> <INCLUDES> <FLAGS> -i <SOURCE> -o <OBJECT>"
@@ -24,11 +27,22 @@ if(MINGW AND UMBA_WINDRES)
         # (сам элемент может не существовать, так как выходной файл, например), то производится его конвертация
         # в короткое имя 8.3 (но в системе это может быть отключено, по умолчанию - включено)
         # UNC/сетевые пути не поддерживаются, но кто в здравом уме их использует при сборке проекта?
-        "<UMBA_WINDRES> <CMAKE_RC_COMPILER> --use-temp-file <FLAGS> -O coff <DEFINES> <INCLUDES> <SOURCE> <OBJECT>"
+        "\$ENV{UMBA_WINDRES} --use-temp-file <FLAGS> -O coff <DEFINES> <INCLUDES> <SOURCE> <OBJECT>"
 
-        PARENT_SCOPE
+        CACHE STRING "Команда компиляции RC с исправлением пробелов" 
+        FORCE  # это главное!
+
+        # PARENT_SCOPE
         )
+    #set(CMAKE_RC_COMPILE_OBJECT 
+    #    "<UMBA_WINDRES> <CMAKE_RC_COMPILER> --use-temp-file <FLAGS> -O coff <DEFINES> <INCLUDES> <SOURCE> <OBJECT>"
+    #    )
+    #set(CMAKE_RC_COMPILE_OBJECT 
+    #    "<UMBA_WINDRES> <CMAKE_RC_COMPILER> --use-temp-file <FLAGS> -O coff <DEFINES> <INCLUDES> <SOURCE> <OBJECT>"
+    #    PARENT_SCOPE
+    #    )
+
 endif()
-message("CMAKE_RC_COMPILE_OBJECT: ${CMAKE_RC_COMPILE_OBJECT}")
+message("umba_fix_windres (2): CMAKE_RC_COMPILE_OBJECT: ${CMAKE_RC_COMPILE_OBJECT}")
 
 endfunction()
